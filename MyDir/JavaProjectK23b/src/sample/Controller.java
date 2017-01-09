79f8b5012fd1a0a8b016d52ca178db92070b4727
LeonidasHliopoulos <mrleonhliop@gmail.com>
2017-01-10 00:34:19 +0200

    selidopoihsh + Refresh(fixed) + Search(fixed)

--- MyDir/JavaProjectK23b/src/sample/Controller.java
+++ MyDir/JavaProjectK23b/src/sample/Controller.java
@@ -1,18 +1,28 @@
 package sample;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 import java.io.IOException;
 import java.sql.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class Controller {
@@ -30,6 +40,8 @@ public class Controller {
     @FXML
     private Label min_check;
     @FXML
+    private Label try_again;
+    @FXML
     private TextField max_light;
     @FXML
     private TextField min_light;
@@ -42,6 +54,8 @@ public class Controller {
     @FXML
     private ChoiceBox Sensor_Type;
     @FXML
+    private TextField Sensor_Value;
+    @FXML
     private DatePicker Date;
     @FXML
     private ChoiceBox cbox_hours;
@@ -49,6 +63,8 @@ public class Controller {
     private ChoiceBox cbox_mins;
     @FXML
     private ChoiceBox cbox_secs;
+    @FXML
+    private Label datetime_check;
 
     public void Apply(ActionEvent mouseEvent) {
 
@@ -67,29 +83,36 @@ public class Controller {
            if (min_light_int < 0 || min_light_int > 10000)
                fMin = true;
        }
-       else
-           fMin = true;
+       else fMin = true;
 
        min_check.setVisible(fMax);
        max_check.setVisible(fMin);
-//       if (fMax || fMin)
-//           return;
 
-        System.out.println("clicked on "+ x_axis.getValue()+"    " + y_axis.getValue()+ "   " + z_axis.getValue()+" "+freq.getValue());
+       if (fMax==true || fMin==true){
+           try_again.setVisible(true);
+       }else
+           System.out.println("clicked on "+ max_light.getText()+"   "+ min_light.getText() +"     " + x_axis.getValue() +"    " + y_axis.getValue() + "   " + z_axis.getValue() +" "+ freq.getValue());
     }
 
 
 //--------------------------------------
-
-    public class Records{
-        int id_name;
-        Float latitude ;
-        Float longitude;
+    class Records {
+        String id_name;
+        float latitude ;
+        float longitude;
         String sensor_type;
-        Float records;
+        float  sensor_value;
         // @Temporal(TemporalType.TIMESTAMP)
-        java.util.Date datetime ;
+        Date  datetime ;
     }
+
+
+   public static List<Records> rec_li = new ArrayList<Records>();
+    ResultSet rs;
+    int resperpage  = 2;
+    int numofpages;
+
+
 
     public void Search(ActionEvent mouseEvent) throws IOException {
         System.out.println("maaaaa");
@@ -112,7 +135,7 @@ public class Controller {
                 String SQL = "SELECT * FROM blind_light_data  ";
                 PreparedStatement prepared_state= connection.prepareStatement(SQL);;
 
-                if ( !user_id.getText().equals("") ||  !longtitude.getText().equals("") || !latitude.getText().equals("") ||  Sensor_Type.getValue()!=null || Date.getValue()!=null ){
+                if ( !user_id.getText().equals("") ||  !longtitude.getText().equals("") || !latitude.getText().equals("") ||  Sensor_Type.getValue()!=null ||  !Sensor_Value.getText().equals("")  || Date.getValue()!=null ){
                     SQL += "where ";
                 }
                 int cur=0;
@@ -124,7 +147,7 @@ public class Controller {
                     else {
                         flag=true;
                     }
-                    SQL += " name=?";
+                    SQL += " user_id=?";
                 }
                 if( !longtitude.getText().equals("")   ){
                     if (flag==true){
@@ -153,6 +176,15 @@ public class Controller {
                     }
                     SQL += "  sensorType=?";
                 }
+                if(!Sensor_Value.getText().equals("")){
+                    if (flag==true){
+                        SQL += " and ";
+                    }
+                    else {
+                        flag=true;
+                    }
+                    SQL += "  sensorValue=?";
+                }
                 if(Date.getValue()!=null){
                     if (flag==true){
                         SQL += " and ";
@@ -180,6 +212,10 @@ public class Controller {
                     cur++;
                     prepared_state.setString(cur,Sensor_Type.getValue().toString() );
                 }
+                if(!Sensor_Value.getText().equals("")){
+                    cur++;
+                    prepared_state.setString(cur,Sensor_Value.getText().toString() );
+                }
 
                 if(Date.getValue()!=null){
                     cur++;
@@ -190,26 +226,77 @@ public class Controller {
                     prepared_state.setTimestamp(cur,mytmp);
                 }
 
-
                 System.out.println(prepared_state);
 
-               ResultSet rs = prepared_state.executeQuery();
-/*
-                Records temp_record = new Records();
+               rs = prepared_state.executeQuery();
+
+
+                // Print results
+               // if (cbox_hours.getValue()==null || cbox_mins.getValue()==null || cbox_secs.getValue()==null ) {
+                //    datetime_check.setVisible(true);
+            //        return;
+             //   }
+
+                Stage primaryStage = Main.getPrimaryStage();
+                primaryStage.setTitle("Result");
+                Pane myPane = FXMLLoader.load(getClass().getResource("Search.fxml"));
+
+                int numofres=0;
+                if (rs.last()) {
+                    numofres = rs.getRow();
+                    rs.beforeFirst();
+                }
+
+                if  (numofres%resperpage>0){
+                    numofpages = numofres/resperpage+1;
+                }
+                else{
+                    numofpages = numofres/resperpage;
+                }
+                if (numofres == 0) {
+                    numofpages=1;
+                }
+
+                rec_li.clear();
                 while (rs.next()) {
-                    temp_record.id_name = rs.getInt("name");
+                    Records temp_record = new Records();
+                    temp_record.id_name = rs.getString("user_id");
                     temp_record.latitude = rs.getFloat("location_latitude");
                     temp_record.longitude = rs.getFloat("location_longitude");
                     temp_record.sensor_type = rs.getString("sensorType");
-                    temp_record.records = rs.getFloat("records");
+                    temp_record.sensor_value = rs.getFloat("sensorValue");
                     // @Temporal(TemporalType.TIMESTAMP)
                     temp_record.datetime = rs.getDate("date_time");
+                    rec_li.add(temp_record);
+                 }
 
-                    // list_records.Add(temp_record);
-                    //items.add(temp_record.id_name + " " + temp_record.latitude + " " + temp_record.longitude + " " + temp_record.sensor_type + " " + temp_record.records + " " + temp_record.datetime + "");
-                    System.out.println(temp_record.id_name + " " + temp_record.latitude + " " + temp_record.longitude + " " + temp_record.sensor_type + " " + temp_record.records + " " + temp_record.datetime + "");
-                }
-                */
+
+                Pagination mypg =  new Pagination();
+                mypg.setPageCount(numofpages);
+
+                mypg.setPageFactory(new Callback<Integer, Node>() {
+                    @Override
+                    public Node call(Integer pageIndex) {
+                        try {
+                            return createPage(pageIndex);
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                            return null;
+                        }
+                    }
+
+                });
+
+
+               Pane sec_pane = (Pane) myPane.getChildren().get(1);
+               sec_pane.getChildren().add(mypg);
+
+
+                Scene scene = new Scene(myPane,1000, 600);
+                primaryStage.close();
+                primaryStage.setScene(scene);
+                primaryStage.show();
+
             }
             catch (SQLException err)    {
                 System.out.println( err.getMessage() );
@@ -217,19 +304,115 @@ public class Controller {
                 e.printStackTrace();
             }
 
-        // Print results
-            Stage primaryStage = Main.getPrimaryStage();
-            primaryStage.setTitle("Result)");
-            TabPane myPane = FXMLLoader.load(getClass().getResource("Search.fxml"));
-            Scene scene = new Scene(myPane);
-            primaryStage.close();
-            primaryStage.setScene(scene);
-            primaryStage.show();
+    }
+
+    public VBox createPage(int pageIndex) throws SQLException {
+        VBox box = new VBox(5);
+        TableView<ObservableList<String>> element = new TableView<>();
+        // add columns
+        List<String> columnNames = new ArrayList<>();
+        columnNames.add("user_id");
+        columnNames.add("latitude");
+        columnNames.add("longitude");
+        columnNames.add("sensor_Type");
+        columnNames.add("sensor_Value");
+        columnNames.add("date_time");
+
+        for (int j = 0; j < columnNames.size(); j++) {
+            final int finalIdx = j;
+            TableColumn<ObservableList<String>, String> column = new TableColumn<>(
+                    columnNames.get(j)
+            );
+            column.setCellValueFactory(param ->
+                    new ReadOnlyObjectWrapper<>(param.getValue().get(finalIdx))
+            );
+            element.getColumns().add(column);
+        }
+
+
+            // add data
+            for (int j = 0; j < resperpage; j++) {
+                if (pageIndex*resperpage+j >= rec_li.size() ){
+                    break;
+                }
+                Records rec =rec_li.get(pageIndex*resperpage+j);
+                if (rec!=null) {
+                    List<String> myar = new ArrayList<>();
+                    String cur_str;
+                    cur_str = rec.id_name;
+                    myar.add(cur_str);
+                    cur_str = Float.toString(rec.latitude);
+                    myar.add(cur_str);
+                    cur_str = Float.toString(rec.longitude);
+                    myar.add(cur_str);
+                    cur_str = rec.sensor_type;
+                    myar.add(cur_str);
+                    cur_str = Float.toString(rec.sensor_value);
+                    myar.add(cur_str);
+                    if (rec.datetime!=null) {
+                        cur_str =rec.datetime.toString();
+                    }
+                    else {
+                        cur_str ="";
+                    }
+                    myar.add(cur_str);
+
+                    element.getItems().add(
+                            FXCollections.observableArrayList(
+                                    myar
+                            )
+                    );
+                }
+            }
+
+        box.getChildren().add(element);
+
+        return box;
     }
 
 
     public void Refresh(ActionEvent mouseEvent) throws IOException {
-       Search(mouseEvent);
+
+        Stage primaryStage = Main.getPrimaryStage();
+        primaryStage.setTitle("Result)");
+        Pane myPane = FXMLLoader.load(getClass().getResource("Search.fxml"));
+
+        Pagination mypg =  new Pagination();
+
+        if  (rec_li.size()%resperpage>0){
+            numofpages = rec_li.size()/resperpage+1;
+        }
+        else{
+            numofpages = rec_li.size()/resperpage;
+        }
+        if (rec_li.size() == 0) {
+            numofpages=1;
+        }
+
+        mypg.setPageCount(numofpages);
+
+        mypg.setPageFactory(new Callback<Integer, Node>() {
+            @Override
+            public Node call(Integer pageIndex) {
+                try {
+                    return createPage(pageIndex);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+        });
+
+
+        Pane sec_pane = (Pane) myPane.getChildren().get(1);
+        sec_pane.getChildren().add(mypg);
+
+
+        Scene scene = new Scene(myPane,600, 600);
+        primaryStage.close();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public void NewSearch(ActionEvent mouseEvent) throws IOException {
--- MyDir/JavaProjectK23b/src/sample/Main.java
+++ MyDir/JavaProjectK23b/src/sample/Main.java
@@ -3,10 +3,14 @@ package sample;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import java.util.Date;
 import java.sql.*;
@@ -18,6 +22,14 @@ import sample.Pub_Sub.MqttSubscriber;
 import java.util.Date;
 import java.sql.*;
 
+/*
+import static sample.Controller.max_light;
+import static sample.Controller.min_light;
+import static sample.Controller.x_axis;
+import static sample.Controller.y_axis;
+import static sample.Controller.z_axis;
+import static sample.Controller.freq;
+*/
 
 
 public class Main extends Application {
@@ -95,8 +107,6 @@ public class Main extends Application {
         launch(args);
         subscriber = new MqttSubscriber();
         subscriber.main();
-
-
         //publisher = new MqttPublisher();
         //publisher.main();
     }
