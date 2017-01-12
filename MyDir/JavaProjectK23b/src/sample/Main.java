02f75632844fa5f09c01823f5984ce99c974396c
LeonidasHliopoulos <mrleonhliop@gmail.com>
2017-01-12 19:33:56 +0200

    Kalitexnikes epembaseis

--- MyDir/JavaProjectK23b/src/sample/Main.java
+++ MyDir/JavaProjectK23b/src/sample/Main.java
@@ -57,39 +57,6 @@ public class Main extends Application {
 
         ObservableList items =
                 FXCollections.observableArrayList();
-        /*
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "MyNewPass");
-            Statement stmt = connection.createStatement( );
-            String SQL = "SELECT * FROM blind_light_data";
-            ResultSet rs = stmt.executeQuery( SQL );
-
-            Records temp_record=new Records();
-            while (rs.next()) {
-                temp_record.id_name = rs.getInt("name");
-                temp_record.latitude = rs.getFloat("location_latitude");
-                temp_record.longitude = rs.getFloat("location_longitude");
-                temp_record.sensor_type = rs.getString("sensorType");
-                temp_record.records = rs.getFloat("records");
-               // @Temporal(TemporalType.TIMESTAMP)
-                temp_record.datetime = rs.getDate("date_time");
-
-               // list_records.Add(temp_record);
-                items.add(temp_record.id_name+" "+ temp_record.latitude+" "+ temp_record.longitude+" "+ temp_record.sensor_type+" "+ temp_record.records+" "+ temp_record.datetime+"");
-                System.out.println( temp_record.id_name+" "+ temp_record.latitude+" "+ temp_record.longitude+" "+ temp_record.sensor_type+" "+ temp_record.records+" "+ temp_record.datetime+"");
-            }
-        }
-        catch ( SQLException err ) {
-            System.out.println( err.getMessage( ) );
-        }
-
-        ListView<String> listView = new ListView<String>();
-        listView.setItems(items);
-        StackPane root = new StackPane();
-        root.getChildren().add(listView);
-        primaryStage.setScene(new Scene(root, 200, 250));
-        primaryStage.show();
-        */
 
         Parent root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
         primaryStage.setTitle("BlindLight(Beta)");
